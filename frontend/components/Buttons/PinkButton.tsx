9b1e60afaa03ef1b0488ffeb675af370ac38eeaa
Alina-02 <alinamontma@gmail.com>
2025-02-17 20:47:04 +0100

    frontend interfaces

--- frontend/components/Buttons/PinkButton.tsx
+++ frontend/components/Buttons/PinkButton.tsx
@@ -1,6 +1,6 @@
 import { View, Text, Pressable } from "react-native";
-import React from "react";
-import { Circle } from "react-native-svg";
+import React, { useState } from "react";
+import Feather from "@expo/vector-icons/Feather";
 
 interface Props {
   text: string;
@@ -8,14 +8,22 @@ interface Props {
 }
 
 export default function PinkButton({ text, onPress }: Props) {
+  const [isInfoModalVisible, setInfoModalVisible] = useState(false);
   return (
     <Pressable
-      className="align-middle justify-center bg-pink_main rounded-xl mt-4 p-4 w-full h-20"
+      className="flex-row items-center justify-between bg-pink_main rounded-xl mt-4 p-4 w-full h-20"
       onPress={onPress}
     >
+      <View style={{ width: 24 }} />
       <Text className="text-center text-xl text-pink_dark font-semibold">
         {text}
       </Text>
+      <Pressable
+        onLongPress={() => setInfoModalVisible(true)}
+        delayLongPress={500}
+      >
+        <Feather name="info" size={24} color="#D164C1" />
+      </Pressable>
     </Pressable>
   );
 }
--- frontend/utils/interfaces.ts
+++ frontend/utils/interfaces.ts
@@ -1,5 +1,27 @@
-export interface Player {
+export interface User {
   name: string;
   points: number;
   group: string;
+  password?: string;
+}
+
+export interface Event {
+  timestamp?: string;
+  user: User;
+  points: number;
+  type: EventType;
+  winner?: User;
+  loser?: User;
+}
+
+export enum EventType {
+  ADD = "ADD",
+  DELETE = "DELETE",
+  BET = "BET",
+}
+
+export interface Group {
+  name: string;
+  user: User[];
+  points: number;
 }
